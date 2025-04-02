a83a0d157dff73189431fb476e474c3b9857c17b
Orest Yakymiv <akimivorest@gmail.com>
2025-04-02 20:44:58 +0300

    checking the word for an isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,30 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "initial_meaning_word, expected_result",
+    [
+        pytest.param(
+            '',
+            True,
+        ),
+
+        pytest.param(
+            "love",
+            True,
+        ),
+
+        pytest.param(
+            "Adam",
+            False,
+        ),
+
+        pytest.param(
+            "baal",
+            False,
+        ),
+    ]
+)
+def test_is_isogram(initial_meaning_word: str, expected_result: bool) -> None:
+    assert is_isogram(initial_meaning_word) == expected_result
